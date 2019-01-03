85714c284e26515cab2124b6f638b871ccdd64a6
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-03 22:17:03 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ function getCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
   req.addEventListener('load', showCommits);
-  req.open('GET', 'https://api.github.com/repos/' + owner + '/' + repo + '/commits'));
+  req.open('GET', 'https://api.github.com/repos/' + owner + '/' + repo + '/commits');
   req.send();
 }
 
