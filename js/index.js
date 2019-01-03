cc0fbd83af90c5e27f07569747c968957d2e2e17
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-03 22:19:03 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -38,12 +38,13 @@ function showCommits() {
   document.getElementById('commits').innerHTML = commitsList;
 }
 
-function getRepositories() {
-  const req = new XMLHttpRequest();
-  req.addEventListener('load');
-  req.open('GET', 'https://api.github.com/users/' + username + '/repos');
-  req.send();
-}
+ function getRepositories() {
+   const username = document.getElementById('username').value;
+   const req = new XMLHttpRequest();
+   req.addEventListener('load', displayRepositories);
+   req.open('GET', 'https://api.github.com/users/' + username + '/repos');
+   req.send();
+ }
 
 function getBranches() {
   
