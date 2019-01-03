1559b7ea9170deca31a9e7948a34fab05f756a82
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-03 22:14:22 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -23,7 +23,20 @@ function getCommits(el) {
   req.send();
 }
 
-
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
+}
 
 function getRepositories() {
   const req = new XMLHttpRequest();
