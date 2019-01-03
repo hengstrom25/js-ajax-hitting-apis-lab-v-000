571cee6285ebcd4782f45a9deaecdcec59837f5e
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-03 22:19:26 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,42 +1,6 @@
 // your code here
 
-function displayCommits() {
-  const commits = JSON.parse(this.responseText);
-  const commitsList = `<ul>${commits
-    .map(
-      commit =>
-        '<li><strong>' +
-        commit.author.login +
-        '</strong> - ' +
-        commit.commit.message +
-        '</li>'
-    )
-    .join('')}</ul>`;
-  document.getElementById('commits').innerHTML = commitsList;  
-}
 
-function getCommits(el) {
-  const name = el.dataset.repo;
-  const req = new XMLHttpRequest();
-  req.addEventListener('load', showCommits);
-  req.open('GET', 'https://api.github.com/repos/' + owner + '/' + repo + '/commits');
-  req.send();
-}
-
-function showCommits() {
-  const commits = JSON.parse(this.responseText);
-  const commitsList = `<ul>${commits
-    .map(
-      commit =>
-        '<li><strong>' +
-        commit.author.login +
-        '</strong> - ' +
-        commit.commit.message +
-        '</li>'
-    )
-    .join('')}</ul>`;
-  document.getElementById('commits').innerHTML = commitsList;
-}
 
  function getRepositories() {
    const username = document.getElementById('username').value;
