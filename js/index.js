519eab9bcf8b7285c2e994ef3eb6ef0e4f4c1dd2
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-03 22:07:37 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 // your code here
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;  
+}
+
+function getCommits() {
+  
+}
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load');
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
+function getBranches() {
+  
+}
+
+
